13e9e85f74305a8140fb0fd3bb27da1c446b5d5c
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-13 03:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal(animal) {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   console.log('cat')
-  return '$animal'
+  return '${animal}'
 }
 
 function add2(n) {
